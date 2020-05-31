e5adeadabb81fd8ee3750526cd8a537cb417e7ba
unknown <jgmejiaco@gmail.com>
2020-05-31 18:54:29 -0500

    Maquetas 1, 2, 3, 4, 11, 12, 15

--- src/app/configs/App.js
+++ src/app/configs/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from "../containers/home/home-page";
 import RegistroClientes from "../containers/registro-clientes/registro-clientes.jsx";
+import ProtectionElements from "../containers/protection-elements/protection-elements.jsx";
 import PreviewPage from "../containers/preview-data/preview-page";
 import PolicyDataPage from  "../containers/policy-treatment-data/policy-treatment-data.page"
 import SavePage from '../containers/save/save.page'
@@ -15,6 +16,7 @@ const App = () => {
               <Switch>
                 <Route exact path= "/home" component={HomePage} />
                 <Route exact path= "/registro-clientes" component={RegistroClientes} />
+                <Route exact path= "/protection-elements" component={ProtectionElements} />
                 <Route exact path= "/preview" component={PreviewPage} />
                 <Route exact path= "/policy_data" component={PolicyDataPage} />
                 <Route exact path= "/save" component={SavePage} />
---
+++ src/app/containers/protection-elements/protection-elements.jsx
@@ -0,0 +1,74 @@
+import React, { useState }  from 'react';
+import Header from '../../components/Header/header.jsx';
+import Footer from '../../components/Footer/footer.jsx';
+import OvalosHome from '../../../assets/images/ovalos-home.svg';
+import PolicyData from '../../containers/policy-treatment-data/policy-treatment-data.page.jsx';
+import './protection-elements.css';
+
+
+const ProtectionElements = () =>{
+
+    return(
+        <React.Fragment>
+            <div className='main-container'>
+            
+                <div className='header'>
+                    <Header/>
+                </div>
+
+                <div className='main-protection-elements-container'>
+                    <form className='main-form-container' action="">
+                        <div className='protection-elements-section'>
+                            <h3 className='protection-elements-title'>lleva elementos de prevención</h3>
+                            <div className='protection-options-container'>
+                                <div className='checkbox-container'>
+                                    <input className='s-checkbox' type="text"/>
+                                    <label className='s-element m-right' htmlFor="">Tapabocas</label>
+                                </div>
+                                
+                                <div className='checkbox-container r-top'>
+                                    <input className='s-checkbox' type="text"/>
+                                    <label className='s-element m-right' htmlFor="">Máscara protectora</label>
+                                </div>
+                                
+                                <div className='checkbox-container r-top'>
+                                    <input className='s-checkbox' type="text"/>
+                                    <label className='s-element' htmlFor="">Guantes</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='options-risk-main-container'>
+                            <h3 className='protection-elements-title'>Seleccione las opciones que apliquen en su caso</h3>
+                            <div className='options-risk-container'>
+                                    <input className='s-checkbox' type="text"/>
+                                    <label className='s-element' htmlFor="">He presentado algún síntoma la última semana (tos, fiebre, malestar general)</label>
+
+                                    <input className='s-checkbox m-top' type="text"/>
+                                    <label className='s-element m-top' htmlFor="">Estuve en contacto con personas que presentaron alguno de esos síntomas</label>
+
+                                    <input className='s-checkbox m-top' type="text"/>
+                                    <label className='s-element m-top' htmlFor="">Hice un viaje internacional en los últimos 30 días</label>
+                                
+                                    <input className='s-checkbox m-top' type="text"/>
+                                    <label className='s-element m-top' htmlFor="">Hice un viaje nacional en los últimos 30 días</label>
+                                
+                                    <input className='s-checkbox m-top' type="text"/>
+                                    <label className='s-element m-top' htmlFor="">Soy trabajador de la salud</label>
+                            </div>
+                        </div>                        
+
+                        <button className=''>registrar</button>
+                    </form>
+                </div>
+
+                <img className='ovalos-protection-elements' src={OvalosHome} alt=""/>
+                
+                <div className='footer'>
+                    <Footer/>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+export default ProtectionElements;
